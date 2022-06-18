d6f028f3d3330cb63b672df800e34e69a09d85c8
Ertondo <gustavoarmitano@gmail.com>
2022-06-18 09:27:29 -0300

    Complete a function of btn_borrar button and add <p> with num of task

--- src/script.js
+++ src/script.js
@@ -1,19 +1,39 @@
 const form_element = document.getElementById("add_item");
+const num_tareas = document.querySelector(".tareas_pendientes");
 
+let num = 0;
+
+//Evento del boton AGREGAR TAREA
 form_element.addEventListener("submit", (event) => {
   event.preventDefault();
   let now_item = document.getElementById("item").value;
-  console.log(now_item);
-  form_element.reset();
 
-  let num = 0;
-  let detail = now_item;
-  let time = new Date();
+  if (now_item) {
+    form_element.reset();
 
-  let now = time.toTimeString().substring(0, 9);
+    num++;
+    let detail = now_item;
+    let time = new Date();
+    //tomo solo los primeros 8 caracteres de la hora
+    let now = time.toTimeString().substring(0, 9);
 
-  insert_rows(num, detail, now);
+    insert_rows(num, detail, now);
+    suma_tarea();
+  }
 });
+
+function suma_tarea() {
+  num_tareas.textContent = "Tiene " + num + " tareas pendientes";
+}
+
+function resta_tarea() {
+  num--;
+  if (num != 0) {
+    num_tareas.textContent = "Tiene " + num + " tareas pendientes";
+  } else {
+    num_tareas.textContent = "No tiene tareas pendientes";
+  }
+}
 
 //Funcion que inserta los datos ingresados en la tabla
 function insert_rows(num, detail, time) {
@@ -22,7 +42,23 @@ function insert_rows(num, detail, time) {
   let row_cell2 = data_storage_table.insertCell(1);
   let row_cell3 = data_storage_table.insertCell(2);
 
-  row_cell1.innerHTML = num;
-  row_cell2.innerHTML = detail;
-  row_cell3.innerHTML = time;
+  const btn_borrar = document.createElement("button");
+  btn_borrar.textContent = "X";
+  btn_borrar.className = "btn_borrar";
+
+  //Agrego el boton de tarea pendiente
+  row_cell3.appendChild(btn_borrar);
+  row_cell1.innerHTML = detail;
+  row_cell2.innerHTML = time;
+
+  //Dos Funciones que escuchan el boton de tarea pendiente
+  btn_borrar.addEventListener("click", (e) => {
+    // Cambia el boton a ok y lo inhabilita
+    // btn_borrar.textContent = "Ok";
+    // btn_borrar.disabled = true;
+
+    //otra forma...borra la tarea
+    data_storage_table.remove();
+    resta_tarea();
+  });
 }
